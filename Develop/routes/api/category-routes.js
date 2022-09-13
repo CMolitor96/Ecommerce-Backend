49b9948f93560f14b845c6cbdea49ad8241ab381
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-09-13 12:57:07 -0700

    Finished connecting sequelize to server so now server works, just had to get rid off app.get and replace with sequelize.sync. Finished working on all models, I have all models with their respective table columns finished, and I believe I correctly implemented the relationships and foreign keys according to the instructions. I then started working on the routes for categories, implementing the get for all categories, a specific category, post a new category, and put a specific category. Still need to write code to delete specific category, and then do routes for products and tags.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,22 +3,74 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    // be sure to include its associated Products
+    res.status(200).json(allCategories)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!updateCategory) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    // await updateCategory.create({
+    //   category_name: req.body.category_name,
+    // });
+    // await updateCategory.save();
+    res.status(200).json(updateCategory)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
